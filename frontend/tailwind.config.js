afdc4218727f21b7ba5a8369f6d6d6d5d0f3e2e9
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-01-14 17:50:08 -0300

    style: add custom color palette to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-green': '#83ded1',
+        'custom-blue': '#a2b1e8',
+        'custom-purple': '#e0abec',
+        'custom-pink': '#f5b3bd',
+      },
+    },
   },
   plugins: [],
 }
