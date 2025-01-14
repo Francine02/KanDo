02199a7812a643e037c96f8771400a4a32925f32
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-01-14 17:57:46 -0300

    feat: component with the image that will be the users' photos

---
+++ frontend/src/components/home/AvatarImage.tsx
@@ -0,0 +1,10 @@
+export default function AvatarImage() {
+    return (
+        <>
+            <img
+                className="w-8 h-8 md:w-11 md:h-11 border-2 border-[#ec8897] rounded-full"
+                src="https://img.icons8.com/?size=100&id=23239&format=png&color=000000"
+                alt="Imagem de perfil" />
+        </>
+    )
+}
