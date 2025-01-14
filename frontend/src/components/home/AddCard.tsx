997d83f927b681a358932e5f946c7e065c40663b
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-01-14 17:58:38 -0300

    feat: component for add task

---
+++ frontend/src/components/home/AddCard.tsx
@@ -0,0 +1,7 @@
+export default function AddCard () {
+    return (
+        <button className="bg-gray-200 hover:bg-gray-300 w-full h-8 rounded-xl mt-2">
+            <p className="text-2xl text-gray-600 font-bold">+</p>
+        </button>
+    )
+}
