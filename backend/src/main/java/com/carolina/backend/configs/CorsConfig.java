325505605352396fe36732021229d11abf928888
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-01-26 15:00:50 -0300

    chore: configure cors for API requests

--- backend/src/main/java/com/carolina/backend/configs/CorsConfig.java
+++ backend/src/main/java/com/carolina/backend/configs/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.carolina.backend.config;
+package com.carolina.backend.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -6,9 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .allowedOrigins(
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
                         "http://localhost:5173",
                         "https://kando-wine.vercel.app")
                 .allowedMethods("*")
