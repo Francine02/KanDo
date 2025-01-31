02331453830358b0cfd237873532adc52856d85f
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-01-31 19:05:01 -0300

    feat: protected route routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Auth } from "./pages/Auth"
 import Home from "./pages/Home"
+import ProtectedRoute from "./components/auth/ProctedRoute";
 
 function App() {
-
   return (
-    // Depois irei arrumar as rotas
-    <>
-      <Auth />
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Auth />} path="/" />
+        <Route element={<ProtectedRoute />} >
+          <Route element={<Home />} path="/home" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
