af38767085be898224d7a521889467a47b2bac98
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-01-31 19:04:01 -0300

    feat: implementing the props

--- frontend/src/components/home/AvatarCircle.tsx
+++ frontend/src/components/home/AvatarCircle.tsx
@@ -1,10 +1,15 @@
 import AvatarImage from "./AvatarImage";
+import UserMenu from "./UserMenu";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function AvatarCircle() {
+    const { user } = useAuth();
+
     return (
         <div className="flex -space-x-3 md:-space-x-4 rtl:space-x-reverse">
-            <AvatarImage />
+            {user && <AvatarImage image={user.image} />}
             <a className="flex items-center justify-center w-8 h-8 md:w-11 md:h-11 text-xs md:text-base font-medium text-white bg-[#ec8897] border-2 border-[#ec8897] rounded-full" href="#">+5</a>
+            {user && <UserMenu name={user.name} email={user.email} />}
         </div>
     )
 }
--- frontend/src/components/home/AvatarImage.tsx
+++ frontend/src/components/home/AvatarImage.tsx
@@ -1,9 +1,11 @@
-export default function AvatarImage() {
+import { UserTypes } from "../../types/UserTypes";
+
+export default function AvatarImage({ image }: UserTypes) {
     return (
         <>
             <img
                 className="w-8 h-8 md:w-11 md:h-11 border-2 border-[#ec8897] rounded-full"
-                src="https://img.icons8.com/?size=100&id=23239&format=png&color=000000"
+                src={image}
                 alt="Imagem de perfil" />
         </>
     )
