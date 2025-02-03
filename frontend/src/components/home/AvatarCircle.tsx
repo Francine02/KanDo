90fd8c053993745bb2a6543e246b728abc3e07cf
Francine02 <francine.c.oliveiracruz@gmail.com>
2025-02-03 18:26:22 -0300

    refactor: separating the userMenu to be used in the settings

--- frontend/src/components/home/AvatarCircle.tsx
+++ frontend/src/components/home/AvatarCircle.tsx
@@ -1,6 +1,5 @@
-import AvatarImage from "./AvatarImage";
-import UserMenu from "./UserMenu";
 import { useAuth } from "../../hooks/useAuth";
+import AvatarImage from "./AvatarImage";
 
 export default function AvatarCircle() {
     const { user } = useAuth();
@@ -9,7 +8,6 @@ export default function AvatarCircle() {
         <div className="flex -space-x-3 md:-space-x-4 rtl:space-x-reverse">
             {user && <AvatarImage image={user.image} />}
             <a className="flex items-center justify-center w-8 h-8 md:w-11 md:h-11 text-xs md:text-base font-medium text-white bg-[#ec8897] border-2 border-[#ec8897] rounded-full" href="#">+5</a>
-            {user && <UserMenu name={user.name} email={user.email} />}
         </div>
     )
 }
